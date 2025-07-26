f12ded3741e9b2cb55a107b9d15a899f26c4f5b4
Alirioaltuve <163304175+Alirioaltuve@users.noreply.github.com>
2025-07-26 16:45:20 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+// service worker
